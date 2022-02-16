1159106f8c68087b6298de01c92f32872eb25d7c
dahye-program <dahye0729@sch.ac.kr>
2022-02-17 01:04:46 +0900

    Modify : Header UI 수정

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,21 +3,56 @@ import styled from 'styled-components';
 
 const Header: React.FC = () => {
   return (
-    <Container>
-      <Wrapper>
-        <Title />
-        {/* <Img src="assets/headerIcon/starIcon.png" /> */}
-      </Wrapper>
-    </Container>
+    <>
+      <Container>
+        <Wrapper>
+          <img src="assets/gif/peanutGif.gif" alt="logo" />
+          <Img src="assets/headerIcon/peanutschoolIcon.png" alt="text" />
+          <ul>
+            <li>도서구매</li>
+            <li>장바구니</li>
+            <li>|</li>
+            <li>이용권 관리</li>
+            <li>로그인/회원가입</li>
+          </ul>
+        </Wrapper>
+      </Container>
+    </>
   );
-}
+};
 
 export default Header;
 
-const Container = styled.section``;
+const Container = styled.header`
+  padding-left: 30px;
+  padding-right: 30px;
+  display: flex;
+  justify-content: center;
+  height: 90px;
+`;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  max-width: 1140px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #ffffff;
+  font-size: 16px;
+  img {
+    width: 47px;
+    height: 30px;
+  }
+  ul {
+    display: flex;
+  }
+  li {
+    list-style-type: none;
+    margin-right: 50px;
+    cursor: pointer;
+  }
+`;
 
-const Title = styled.span``;
-
-const Img = styled.img``;
+const Img = styled.img`
+  width: 47px;
+  height: 30px;
+`;
